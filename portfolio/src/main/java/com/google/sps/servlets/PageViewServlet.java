47cac5ed101e95532e9b097be9a4f3516d12b336
zorazrr <zorazrr@gmail.com>
2022-06-06 05:37:40 +0000

    set up new servlet

--- portfolio/src/main/java/com/google/sps/servlets/PageViewServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/PageViewServlet.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+/** Servlet that returns HTML that contains the page view count. */
 @WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+public class PageViewServlet extends HttpServlet {
+
+  private int pageViews = 0;
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    pageViews++;
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello World</h1>");
+    response.getWriter().println("<p>Hello There!</p>");
+    response.getWriter().println("<p>This page has been viewed " + pageViews + " times.</p>");
   }
 }
